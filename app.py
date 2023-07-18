0c0ae1aeccd22060bcac326948c8c40fcfcbd117
Etizaz Shah <139033592+etizazshah@users.noreply.github.com>
2023-07-17 23:00:40 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+# !pip install scikit-learn nltk matplotlib pandas gensim joblib --quiet
 import random
 import time
 import joblib
@@ -5,86 +6,63 @@ import pickle
 import streamlit as st
 from preprocessor import preprocess_text
 import nltk
-
-# Load NLTK resources
 nltk.download('punkt')
 nltk.download('wordnet')
 
-# Function to load the vectorizer
-@st.cache(allow_output_mutation=True)
-def load_vectorizer():
+
+@st.cache_resource
+def vec():
+    # Load the saved model from a file
     with open("./Models/count_vectorizer.pkl", "rb") as f:
         vectorizer = pickle.load(f)
     return vectorizer
 
-# Function to load the Naive Bayes model
-@st.cache(allow_output_mutation=True)
-def load_model():
+text_vectorizer = vec()
+
+@st.cache_resource
+def get_model():
     model = joblib.load("./Models/naive_model.pkl")
     return model
 
-# Initialize the vectorizer and model
-text_vectorizer = load_vectorizer()
-naive_model = load_model()
+naive_model = get_model()
 
 # Building the front end
+
 st.title("Sentiment Bot")
 
 # Initialize chat history
 if "messages" not in st.session_state:
     st.session_state.messages = []
 
-# Create a random color generator for assistant responses
-def get_random_color():
-    return "#{:06x}".format(random.randint(0, 0xFFFFFF))
-
-# Assistant responses for different sentiment classes
-positive_responses = [
-    "I sense a positive sentiment!",
-    "Your message exudes positivity!",
-    "A burst of sunshine! Your sentiment is clearly positive!",
-    "You're spreading positive vibes! Well done!",
-]
-negative_responses = [
-    "I sense a negative sentiment in your message.",
-    "Your message seems negative.",
-    "Let's turn things around! Your sentiment appears negative.",
-    "Sending virtual hugs! Your message conveys a negative sentiment.",
-]
-neutral_responses = [
-    "Hmm, your message is quite neutral. Keep expressing!",
-    "A balanced sentiment! Your message is neither too positive nor too negative.",
-    "You're keeping it cool! A neutral sentiment detected.",
-    "Your message is like a calm breeze - neutral and composed.",
-]
+# Display chat messages from history on app rerun
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
 
 # React to user input
-if prompt := st.text_input("Enter your text here"):
-    # Display user message in chat container
-    st.text_area("User:", prompt, height=100, max_chars=None, key=None)
-
+if prompt := st.chat_input("Enter your text here"):
+    # Display user message in chat message container
+    st.chat_message("user").markdown(prompt)
     # Add user message to chat history
     st.session_state.messages.append({"role": "user", "content": prompt})
 
-    # Analyze sentiment using the model
+    # Analyze sentiment using your model
     new_text = preprocess_text(prompt)
-    new_text = [new_text]  # Convert to a list of strings
-    new_text_vec = text_vectorizer.transform(new_text)  # Transform directly
-    sentiment = naive_model.predict(new_text_vec)
+    new_text = text_vectorizer.transform(new_text).toarray()
+    sentiment = naive_model.predict(new_text)  # Replace `predict()` with the appropriate method for sentiment analysis
 
-    # Determine sentiment label and corresponding response
+    # Determine sentiment label
     if sentiment[0] == 1:
-        response = random.choice(positive_responses)
-        color = get_random_color()
+        response = "I sense a positive sentiment!"
+        print(sentiment)
     elif sentiment[0] == 0:
-        response = random.choice(negative_responses)
-        color = get_random_color()
+        response = "I sense a negative sentiment!"
+        print(sentiment[0])
     else:
-        response = random.choice(neutral_responses)
-        color = get_random_color()
+        response = "I'm not sure about the sentiment."
 
-    # Display assistant response in chat container with colored text
-    st.text_area("Assistant:", response, height=100, max_chars=None, value=None, color=color)
-
+    # Display assistant response in chat message container
+    with st.chat_message("assistant"):
+        st.markdown(response)
     # Add assistant response to chat history
     st.session_state.messages.append({"role": "assistant", "content": response})
